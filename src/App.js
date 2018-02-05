94d574f2717c5523f95651a6dbcc93c87ed810dc
Cesar Moreno <cesar200526@gmail.com>
2018-02-04 21:23:27 -0500

    Lists & Key

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import ToggleButton from './components/ToggleButton'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Cesar', age: 21 },
-      { name: 'Mari', age: 22 },
-      { name: 'Luis1', age: 23 },
-      { name: 'Luis2', age: 24 },
-      { name: 'Luis3', age: 25 },
-      { name: 'Luis4', age: 26 }
+      { id: '123', name: 'Cesar', age: 21 },
+      { id: '234', name: 'Mari', age: 22 },
+      { id: '345', name: 'Luis1', age: 23 },
+      { id: '456', name: 'Luis2', age: 24 },
+      { id: '567', name: 'Luis3', age: 25 },
+      { id: '678', name: 'Luis4', age: 26 }
     ],
     index: '',
     switchPerson: false
@@ -46,12 +46,12 @@ class App extends Component {
   restoreNameHandler = () => {
     this.setState({
       persons: [
-        { name: 'Cesar', age: 21 },
-        { name: 'Mari', age: 22 },
-        { name: 'Luis1', age: 23 },
-        { name: 'Luis2', age: 24 },
-        { name: 'Luis3', age: 25 },
-        { name: 'Luis4', age: 26 }
+        { id: '123', name: 'Cesar', age: 21 },
+        { id: '234', name: 'Mari', age: 22 },
+        { id: '345', name: 'Luis1', age: 23 },
+        { id: '456', name: 'Luis2', age: 24 },
+        { id: '567', name: 'Luis3', age: 25 },
+        { id: '678', name: 'Luis4', age: 26 }
       ]
     })
   }
@@ -69,10 +69,10 @@ class App extends Component {
           {this.state.persons.map((person, index) => {
             return (
               <Person
-                key={index}
+                key={person.id}
                 name={person.name}
                 age={person.age}
-                click={this.deleteItemHandler.bind(this, index)}
+                click={() => this.deleteItemHandler(index)}
               />
             )
           })}
