111d127e40eac5ac1e92ab818694dc0eff2ef8a0
Cesar Moreno <cesar200526@gmail.com>
2018-02-24 17:20:27 -0500

    agrego component style

---
+++ src/containers/Style.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import '../css/Style.css'
+
+class Style extends Component {
+  render () {
+    return (
+      <div>
+        <h1 class="titulo">Style</h1>
+      </div>
+    )
+  }
+}
+
+export default Style
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './containers/App'
+// import App from './containers/App'
 // import StaticSite from './components/Examples/Router'
+import Style from './containers/Style.js'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Style />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
 // ReactDOM.render(<StaticSite />, document.getElementById('root'))
 registerServiceWorker()
