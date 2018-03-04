648aa679372df590529ee045cd0de4a66d20a198
Cesar Moreno <cesar200526@gmail.com>
2018-03-04 08:28:22 -0500

    add flex

---
+++ src/containers/Flex.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react'
+import '../css/Flex.css'
+
+class Flex extends Component {
+  render () {
+    return (
+      <div>
+        <div className="container">
+          <div className="item">1</div>
+          <div className="item">2</div>
+          <div className="item">3</div>
+          <div className="item">4</div>
+          <div className="item">5</div>
+          <div className="item">6</div>
+          {/* <div className="item">7</div>
+          <div className="item">8</div>
+          <div className="item">9</div>
+          <div className="item">10</div>
+          <div className="item">11</div>
+          <div className="item">12</div> */}
+          {/* <div className="item">13</div>
+          <div className="item">14</div>
+          <div className="item">15</div>
+          <div className="item">16</div>
+          <div className="item">17</div>
+          <div className="item">18</div>
+          <div className="item">19</div>
+          <div className="item">20</div>
+          <div className="item">21</div>
+          <div className="item">22</div>
+          <div className="item">23</div>
+          <div className="item">24</div>
+          <div className="item">25</div>
+          <div className="item">26</div>
+          <div className="item">27</div>
+          <div className="item">28</div>
+          <div className="item">29</div>
+          <div className="item">30</div>
+          <div className="item">31</div>
+          <div className="item">32</div>
+          <div className="item">33</div>
+          <div className="item">34</div>
+          <div className="item">35</div>
+          <div className="item">36</div>
+          <div className="item">37</div>
+          <div className="item">38</div>
+          <div className="item">39</div>
+          <div className="item">40</div>
+          <div className="item">41</div>
+          <div className="item">42</div>
+          <div className="item">43</div>
+          <div className="item">44</div>
+          <div className="item">45</div>
+          <div className="item">46</div>
+          <div className="item">47</div>
+          <div className="item">48</div>
+          <div className="item">49</div>
+          <div className="item">50</div> */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Flex;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import './index.css'
 // import StaticSite from './components/Examples/Router'
 // import Style from './containers/Style.js'
 // import BoxModel from './containers/BoxModel.js'
-import Position from './containers/Position.js'
+// import Position from './containers/Position.js'
+import Flex from './containers/Flex'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Position />, document.getElementById('root'))
+ReactDOM.render(<Flex />, document.getElementById('root'))
+// ReactDOM.render(<Position />, document.getElementById('root'))
 // ReactDOM.render(<BoxModel />, document.getElementById('root'))
 // ReactDOM.render(<Style />, document.getElementById('root'))
 // ReactDOM.render(<App />, document.getElementById('root'))
