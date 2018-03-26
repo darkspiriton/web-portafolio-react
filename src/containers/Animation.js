f73453f570f2e5297a0b2e128732dcd385047258
Cesar Moreno <cesar200526@gmail.com>
2018-03-25 21:26:38 -0500

    add animation

---
+++ src/containers/Animation.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import '../css/Animation.css'
+
+class Animation extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Animation</h1>
+        <div className="container">
+          <div className="box">
+          </div>
+        </div>
+        <div className="star-wars">
+        </div>
+        <div className="animate-container">
+        </div>
+        <div className="corazon"></div>
+        <div className="point"></div>
+      </div>
+    )
+  }
+}
+
+export default Animation
